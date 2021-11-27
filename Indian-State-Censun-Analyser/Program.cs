1f2bdadab876bc8d3a8bbfb788dd01782408b6d2
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-27 05:39:55 +0530

    Indian State Censun Analyses Code add

---
+++ Indian-State-Censun-Analyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indian_State_Censun_Analyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
